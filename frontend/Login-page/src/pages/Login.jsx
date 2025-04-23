fe25dc42a0332d2b01a14b641ff0e3a5e8ee098e
CHAMRTHI VENKATESH <160413645+venkatesh767408@users.noreply.github.com>
2025-04-23 22:24:12 +0530

    Update Login.jsx

--- frontend/Login-page/src/pages/Login.jsx
+++ frontend/Login-page/src/pages/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch('https://login-auth-1-api.vercel.app//auth/login', {
+      const response = await fetch('https://login-auth-1-api.vercel.app/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(loginInfo),
